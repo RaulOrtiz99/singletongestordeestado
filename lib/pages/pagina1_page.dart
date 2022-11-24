ea592e786db135af93dfc241ce531fe7fb0ab37f
raulalbertoortiz290199@hotmail.com <RaulOrtiz99>
2022-11-24 16:20:06 -0400

    mostrando la informacion del usuario en un cubit

--- lib/pages/pagina1_page.dart
+++ lib/pages/pagina1_page.dart
@@ -1,4 +1,5 @@
 import 'package:estados_s/bloc/usuario/usuario_cubit.dart';
+import 'package:estados_s/models/usuario.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -11,13 +12,12 @@ class Pagina1Page extends StatelessWidget {
       appBar: AppBar(title: Text("Pagina1")),
       body: BlocBuilder<UsuarioCubit, UsuarioState>(
         builder: (_, state) {
-          print(state);
           if (state is UsuarioInitial) {
-            return Center(
-              child: Text("No hay informacion del usuario"),
-            );
+            return Center(child: Text('No hay información del usuario'));
+          } else if (state is UsuarioActivo) {
+            return InformacionUsuario(usuario: state.usuario);
           } else {
-            return InformacionUsuario();
+            return Center(child: Text('Estado no reconocido '));
           }
         },
       ),
@@ -33,9 +33,9 @@ class Pagina1Page extends StatelessWidget {
 }
 
 class InformacionUsuario extends StatelessWidget {
-  const InformacionUsuario({
-    Key? key,
-  }) : super(key: key);
+  final Usuario usuario;
+
+  const InformacionUsuario({required this.usuario});
 
   @override
   Widget build(BuildContext context) {
@@ -44,15 +44,15 @@ class InformacionUsuario extends StatelessWidget {
       width: double.infinity,
       padding: EdgeInsets.all(20.0),
       child: Column(
-        children: const [
+        children: [
           Text("General",
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
           Divider(),
           ListTile(
-            title: Text("Nombre:"),
+            title: Text('Nombre:${usuario.nombre} '),
           ),
           ListTile(
-            title: Text("Edad:"),
+            title: Text("Edad: ${usuario.edad} "),
           ),
           Text(
             "Profesiones",
